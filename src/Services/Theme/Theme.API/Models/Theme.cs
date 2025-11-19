153eea53463f9231ba2e7250d7ea56c8be45cccf
Anthony Piccirilli <anthony.n.piccirilli.ctr@army.mil>
2025-11-19 13:08:42 -0500

    - Created Theme API

---
+++ src/Services/Theme/Theme.API/Models/Theme.cs
@@ -0,0 +1,15 @@
+﻿namespace Theme.API.Models;
+
+public class Theme
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Number { get; set; } = default!;
+    public string Letter { get; set; } = default!;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; } = default!;
+    public DateTime ModifiedDate { get; set; } = default!;
+    public string ModifiedBy { get; set;} = default!;
+}
